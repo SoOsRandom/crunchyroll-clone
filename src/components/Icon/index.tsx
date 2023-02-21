ab0ba53f600e9c379e300aa4b8e73d9f0f836795
Rian <rianfiore@gmail.com>
2023-02-21 16:51:24 -0300

    feat: adicionando componente Icon ao projeto

--- src/components/Icon/index.tsx
+++ src/components/Icon/index.tsx
@@ -1,3 +1,9 @@
-export function Icon() {
-  return <></>
+import * as icons from 'icons'
+import { useContext } from 'react'
+import { ThemeContext } from 'styles/theme/context'
+import { IconProps } from './types'
+
+export function Icon({ name, size, color }: IconProps) {
+  const { theme } = useContext(ThemeContext)
+  return <>{icons[name]({ size, color: color ?? theme.colors.brand.supportSecondary })}</>
 }
