99597d250b0509d88f3417e3a0a33ae178c27a6a
Rian <rianfiore@gmail.com>
2023-02-21 16:48:56 -0300

    feat: adicionando componente Bookmark

---
+++ public/icons/bookmark/index.tsx
@@ -0,0 +1,8 @@
+import { IconAttributesProps } from 'icons/types'
+import * as React from 'react'
+
+export const Bookmark = ({ size = 24, color, ...props }: IconAttributesProps) => (
+  <svg width={size} height={size} fill="none" xmlns="http://www.w3.org/2000/svg" {...props}>
+    <path d="m19 21-7-5-7 5V5a2 2 0 0 1 2-2h10a2 2 0 0 1 2 2v16Z" stroke={color} strokeWidth={2} strokeLinecap="round" strokeLinejoin="round" />
+  </svg>
+)
