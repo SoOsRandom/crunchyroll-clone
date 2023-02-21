cf605d5b1724e1832c1bfe34792193be3c83af6f
Rian <rianfiore@gmail.com>
2023-02-21 11:29:17 -0300

    chore: configurações inniciais do projeto

---
+++ pages/_app.tsx
@@ -0,0 +1,12 @@
+import type { AppProps } from "next/app";
+import { GlobalReset } from "styles/globalReset";
+import { ThemeProvider } from "styles/theme/context";
+
+export default function App({ Component, pageProps }: AppProps) {
+  return (
+    <ThemeProvider>
+      <GlobalReset />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
+}
